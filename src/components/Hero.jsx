eac517d9341ae6a52d4536e75895f2bba3a2cb46
abdo-elmorsi <abdelrahmandiv@gmail.com>
2024-02-24 15:13:27 +0200

    improve seo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
       <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
+        <a href='#about' aria-label="scroll-to-about">
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{
